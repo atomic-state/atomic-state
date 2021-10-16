bdfeb90c65434e62b5136b316a6e48a01bfc311b
danybeltran <josuebeltran_2010@hotmail.com>
2021-10-15 19:13:15 -0600

    adds license

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 "use strict";
+/**  @license Atomic State
+ * Copyright (c) Dany Beltran
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
