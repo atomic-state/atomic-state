ee64a528eaa0ceedad132add97f4e027aed57b6f
danybeltran <josuebeltran_2010@hotmail.com>
2022-05-03 00:25:38 -0600

    update tests to reset atoms before testing

--- mocks/Tes1.tsx
+++ mocks/Tes1.tsx
@@ -1,22 +1,23 @@
-import React from "react";
-import { atom, useDispatch, useValue } from "../";
-const clicks = atom({
-  name: "clicks-count",
-  default: 0,
-});
+import React, { useEffect } from "react"
+import { useDispatch, useValue } from "../"
+import { clicks } from "./atoms"
 
 export const RenderCount = () => {
-  const clicksCount = useValue(clicks);
-  return <h2>count is {clicksCount}</h2>;
-};
+  const clicksCount = useValue(clicks)
+  return <h2>count is {clicksCount}</h2>
+}
 
 export const IncreaseButton = () => {
-  const setAtomValue = useDispatch(clicks);
+  const setAtomValue = useDispatch(clicks)
+  useEffect(() => {
+    // Reset count when mounting to prevent conflicts with other tests
+    setAtomValue(0)
+  }, [])
   return (
     <div>
       <button onClick={() => setAtomValue((value) => value + 1)}>
         increase
       </button>
     </div>
-  );
-};
+  )
+}
